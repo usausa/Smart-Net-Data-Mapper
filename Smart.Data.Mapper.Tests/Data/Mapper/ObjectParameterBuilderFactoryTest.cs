04b8afad4ef2ded279a6b55fa5fee23360613c58
usausa <machi.pon@gmail.com>
2019-03-25 09:59:00 +0900

    Update tests

---
+++ Smart.Data.Mapper.Tests/Data/Mapper/ObjectParameterBuilderFactoryTest.cs
@@ -0,0 +1,109 @@
+namespace Smart.Data.Mapper
+{
+    using System;
+    using System.Data;
+    using System.Linq;
+
+    using Smart.Data.Mapper.Attributes;
+    using Smart.Mock.Data;
+
+    using Xunit;
+
+    public class ObjectParameterBuilderFactoryTest
+    {
+        [Fact]
+        public void BuildParameter()
+        {
+            using (var con = new MockDbConnection())
+            {
+                var cmd = new MockDbCommand();
+                cmd.SetupResult(0);
+                cmd.Executing = ec =>
+                {
+                    Assert.Equal(13, ec.Parameters.Count);
+                    Assert.Equal(1, ec.Parameters.First(x => x.ParameterName == nameof(Parameter.Value1)).Value);
+                    Assert.Equal(1, ec.Parameters.First(x => x.ParameterName == nameof(Parameter.Value2)).Value);
+                    Assert.Equal(DBNull.Value, ec.Parameters.First(x => x.ParameterName == nameof(Parameter.Value3)).Value);
+                    Assert.Equal(Value.One, ec.Parameters.First(x => x.ParameterName == nameof(Parameter.Value4)).Value);
+                    Assert.Equal(Value.One, ec.Parameters.First(x => x.ParameterName == nameof(Parameter.Value5)).Value);
+                    Assert.Equal(DBNull.Value, ec.Parameters.First(x => x.ParameterName == nameof(Parameter.Value6)).Value);
+
+                    ec.Parameters.First(x => x.ParameterName == nameof(Parameter.Output1)).Value = 1;
+                    ec.Parameters.First(x => x.ParameterName == nameof(Parameter.Output2)).Value = DBNull.Value;
+                    ec.Parameters.First(x => x.ParameterName == nameof(Parameter.Output3)).Value = 1;
+                    ec.Parameters.First(x => x.ParameterName == nameof(Parameter.Output4)).Value = DBNull.Value;
+                    ec.Parameters.First(x => x.ParameterName == nameof(Parameter.Output5)).Value = 1;
+                    ec.Parameters.First(x => x.ParameterName == nameof(Parameter.Output6)).Value = DBNull.Value;
+                    ec.Parameters.First(x => x.ParameterName == nameof(Parameter.Output7)).Value = 1;
+                };
+                con.SetupCommand(cmd);
+
+                var parameter = new Parameter
+                {
+                    Value1 = 1,
+                    Value2 = 1,
+                    Value3 = null,
+                    Value4 = Value.One,
+                    Value5 = Value.One,
+                    Value6 = null
+                };
+                con.Execute("TEST", parameter, commandType: CommandType.StoredProcedure);
+
+                Assert.Equal(1, parameter.Output1);
+                Assert.Equal(0, parameter.Output2);
+                Assert.Equal(1, parameter.Output3);
+                Assert.Null(parameter.Output4);
+                Assert.Equal(Value.One, parameter.Output5);
+                Assert.Null(parameter.Output6);
+                Assert.Equal("1", parameter.Output7);
+            }
+        }
+
+        protected class Parameter
+        {
+            public int Value1 { get; set; }
+
+            public int? Value2 { get; set; }
+
+            public int? Value3 { get; set; }
+
+            public Value Value4 { get; set; }
+
+            public Value? Value5 { get; set; }
+
+            public Value? Value6 { get; set; }
+
+            public int Value7 { set => Value7 = value; }
+
+            [Ignore]
+            public int Value8 { get; set; }
+
+            [Direction(ParameterDirection.Output)]
+            public int Output1 { get; set; }
+
+            [Direction(ParameterDirection.Output)]
+            public int Output2 { get; set; }
+
+            [Direction(ParameterDirection.Output)]
+            public int? Output3 { get; set; }
+
+            [Direction(ParameterDirection.Output)]
+            public int? Output4 { get; set; }
+
+            [Direction(ParameterDirection.Output)]
+            public Value Output5 { get; set; }
+
+            [Direction(ParameterDirection.Output)]
+            public Value? Output6 { get; set; }
+
+            [Direction(ParameterDirection.Output)]
+            public string Output7 { get; set; }
+        }
+
+        protected enum Value
+        {
+            Zero = 0,
+            One = 1
+        }
+    }
+}
--- Smart.Data.Mapper.Tests/Data/Mapper/ObjectResultMapperFactoryTest.cs
+++ Smart.Data.Mapper.Tests/Data/Mapper/ObjectResultMapperFactoryTest.cs
@@ -24,11 +24,13 @@ namespace Smart.Data.Mapper
                     new MockColumn(typeof(int), "Column4"),
                     new MockColumn(typeof(int), "Column5"),
                     new MockColumn(typeof(int), "Column6"),
+                    new MockColumn(typeof(int), "Column7"),
+                    new MockColumn(typeof(int), "Column8"),
                 };
                 var values = new List<object[]>
                 {
-                    new object[] { 1, 1, 1, 1, 1, 1 },
-                    new object[] { DBNull.Value, DBNull.Value, DBNull.Value, DBNull.Value, DBNull.Value, DBNull.Value }
+                    new object[] { 1, 1, 1, 1, 1, 1, 1, 1 },
+                    new object[] { DBNull.Value, DBNull.Value, DBNull.Value, DBNull.Value, DBNull.Value, DBNull.Value, DBNull.Value, DBNull.Value }
                 };
 
                 var reader = new MockDataReader(columns, values);
@@ -40,13 +42,17 @@ namespace Smart.Data.Mapper
                 Assert.Equal(1, list[0].Column1);
                 Assert.Equal(1, list[0].Column2);
                 Assert.Equal(1, list[0].Column3);
-                Assert.Equal(0, list[0].Column4);
-                Assert.Equal(0, list[0].Column5);
+                Assert.Equal(Value.One, list[0].Column4);
+                Assert.Equal(Value.One, list[0].Column5);
+                Assert.Equal(0, list[0].Column6);
+                Assert.Equal(0, list[0].Column7);
                 Assert.Equal(0, list[1].Column1);
                 Assert.Null(list[1].Column2);
                 Assert.Equal(0, list[1].Column3);
-                Assert.Equal(0, list[1].Column4);
-                Assert.Equal(0, list[1].Column5);
+                Assert.Equal(Value.Zero, list[1].Column4);
+                Assert.Null(list[1].Column5);
+                Assert.Equal(0, list[1].Column6);
+                Assert.Equal(0, list[1].Column7);
             }
         }
 
@@ -58,10 +64,20 @@ namespace Smart.Data.Mapper
 
             public long Column3 { get; set; }
 
-            public int Column4 => 0;
+            public Value Column4 { get; set; }
+
+            public Value? Column5 { get; set; }
+
+            public int Column6 => 0;
 
             [Ignore]
-            public int Column5 { get; set; }
+            public int Column7 { get; set; }
+        }
+
+        protected enum Value
+        {
+            Zero = 0,
+            One = 1
         }
     }
 }
