c4e0bed577adfc7193e324e4bd6515b6db071054
usausa <machi.pon@gmail.com>
2019-04-08 19:51:36 +0900

    Fix comment

--- Example/DataAccess.FormsApp/DataAccess.FormsApp.iOS/GlobalSuppressions.cs
+++ Example/DataAccess.FormsApp/DataAccess.FormsApp.iOS/GlobalSuppressions.cs
@@ -1,1 +1,1 @@
-[assembly: System.Diagnostics.CodeAnalysis.SuppressMessage("StyleCop.CSharp.NamingRules", "SA1300:Element must begin with upper-case letter", Justification = "<保留中>", Scope = "namespace", Target = "~N:DataAccess.FormsApp.iOS")]
+[assembly: System.Diagnostics.CodeAnalysis.SuppressMessage("StyleCop.CSharp.NamingRules", "SA1300:Element must begin with upper-case letter", Justification = "Ignore", Scope = "namespace", Target = "~N:DataAccess.FormsApp.iOS")]
