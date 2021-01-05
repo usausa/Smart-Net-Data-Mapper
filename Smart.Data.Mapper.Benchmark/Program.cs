0242b30d650dc1dcdf1467b1a9dd701ce8e76b82
usausa <machi.pon@gmail.com>
2021-01-05 19:50:15 +0900

    Update benchmark

--- Smart.Data.Mapper.Benchmark/Program.cs
+++ Smart.Data.Mapper.Benchmark/Program.cs
@@ -5,9 +5,11 @@ namespace Smart.Data.Mapper.Benchmark
     using System.Linq;
 
     using BenchmarkDotNet.Attributes;
+    using BenchmarkDotNet.Columns;
     using BenchmarkDotNet.Configs;
     using BenchmarkDotNet.Diagnosers;
     using BenchmarkDotNet.Exporters;
+    using BenchmarkDotNet.Exporters.Csv;
     using BenchmarkDotNet.Jobs;
     using BenchmarkDotNet.Running;
 
@@ -25,8 +27,18 @@ namespace Smart.Data.Mapper.Benchmark
     {
         public BenchmarkConfig()
         {
-            AddExporter(MarkdownExporter.Default, MarkdownExporter.GitHub);
+            AddColumn(
+                StatisticColumn.Mean,
+                StatisticColumn.Min,
+                StatisticColumn.Max,
+                StatisticColumn.P90,
+                StatisticColumn.Error,
+                StatisticColumn.StdDev);
             AddDiagnoser(MemoryDiagnoser.Default);
+            AddExporter(MarkdownExporter.Default, MarkdownExporter.GitHub);
+            AddExporter(CsvExporter.Default);
+            //AddExporter(CsvMeasurementsExporter.Default);
+            //AddExporter(RPlotExporter.Default);
             AddJob(Job.LongRun);
         }
     }
