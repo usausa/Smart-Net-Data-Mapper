7833a4445f7cdb0182e711b34a7bb5e80a49602a
usausa <machi.pon@gmail.com>
2021-01-10 19:04:16 +0900

    Add map type test

---
+++ Smart.Data.Mapper.Tests/Data/Mapper/MapTypeTest.cs
@@ -0,0 +1,61 @@
+namespace Smart.Data.Mapper
+{
+    using Microsoft.Data.Sqlite;
+
+    using Xunit;
+
+    public class MapTypeTest
+    {
+        //--------------------------------------------------------------------------------
+        // Record
+        //--------------------------------------------------------------------------------
+
+        [Fact]
+        public void MapRecord()
+        {
+            using var con = new SqliteConnection("Data Source=:memory:");
+            con.Open();
+            con.Execute("CREATE TABLE IF NOT EXISTS Data (Id int PRIMARY KEY, Name text)");
+            con.Execute("INSERT INTO Data (Id, Name) VALUES (1, 'test1')");
+
+            var entity = con.QueryFirstOrDefault<RecordEntity>("SELECT * FROM Data WHERE Id = @Id", new { Id = 1 });
+
+            Assert.NotNull(entity);
+            Assert.Equal(1, entity!.Id);
+            Assert.Equal("test1", entity.Name);
+        }
+
+        protected record RecordEntity
+        {
+            public long Id { get; set; }
+
+            public string? Name { get; set; }
+        }
+
+        //--------------------------------------------------------------------------------
+        // Record
+        //--------------------------------------------------------------------------------
+
+        [Fact]
+        public void MapInitOnly()
+        {
+            using var con = new SqliteConnection("Data Source=:memory:");
+            con.Open();
+            con.Execute("CREATE TABLE IF NOT EXISTS Data (Id int PRIMARY KEY, Name text)");
+            con.Execute("INSERT INTO Data (Id, Name) VALUES (1, 'test1')");
+
+            var entity = con.QueryFirstOrDefault<InitOnlyEntity>("SELECT * FROM Data WHERE Id = @Id", new { Id = 1 });
+
+            Assert.NotNull(entity);
+            Assert.Equal(1, entity!.Id);
+            Assert.Equal("test1", entity.Name);
+        }
+
+        protected class InitOnlyEntity
+        {
+            public long Id { get; init; }
+
+            public string? Name { get; init; }
+        }
+    }
+}
