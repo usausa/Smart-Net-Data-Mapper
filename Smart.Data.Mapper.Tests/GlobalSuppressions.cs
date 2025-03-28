7122c61638f77617524e08c8bbe786cc307a93b2
usausa <machi.pon@gmail.com>
2025-03-28 11:06:11 +0900

    Update test

--- Smart.Data.Mapper.Tests/GlobalSuppressions.cs
+++ Smart.Data.Mapper.Tests/GlobalSuppressions.cs
@@ -1,3 +1,4 @@
 [assembly: System.Diagnostics.CodeAnalysis.SuppressMessage("Maintainability", "CA1515:Consider making public types internal", Justification = "Ignore")]
+[assembly: System.Diagnostics.CodeAnalysis.SuppressMessage("Performance", "CA1812:AvoidUninstantiatedInternalClasses", Justification = "Ignore")]
 [assembly: System.Diagnostics.CodeAnalysis.SuppressMessage("Performance", "CA1849:CallAsyncMethodsWhenInAnAsyncMethod", Justification = "Ignore")]
 [assembly: System.Diagnostics.CodeAnalysis.SuppressMessage("Reliability", "CA2007:Do not directly await a Task", Justification = "Ignore")]
